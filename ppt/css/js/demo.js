294c56dcecec1fef381cd0bc6a5e1d685c5ce85b
wangdaodao <hi@wangdaodao.com>
2013-05-06 10:32:42 +0800

    css
    css

--- ppt/css/js/demo.js
+++ ppt/css/js/demo.js
@@ -39,58 +39,11 @@ jQuery(document).ready(function($) {
     		var demo_relative =$(this).next().attr("style");
     		$(this).next().html(demo_relative);
   	});
-  	$(".float_1l").toggle(
+  	$(".float_l").toggle(
   		function () {
-    		$(".float1").addClass("fleft");
-    		$(".float_1l").html("1号无浮动")
+    		$(this).siblings(".float_demo").addClass("fleft");
  		},
   		function () {
-    		$(".float1").removeClass("fleft");
-    		$(".float_1l").html("1号左浮动")
-  	});
-  	$(".float_1r").toggle(
-  		function () {
-    		$(".float1").addClass("fright");
-    		$(".float_1r").html("1号无浮动")
- 		},
-  		function () {
-    		$(".float1").removeClass("fright");
-    		$(".float_1r").html("1号右浮动")
-  	});
-  	$(".float_2l").toggle(
-  		function () {
-    		$(".float2").addClass("fleft");
-    		$(".float_2l").html("2号无浮动")
- 		},
-  		function () {
-    		$(".float2").removeClass("fleft");
-    		$(".float_2l").html("2号左浮动")
-  	});
-  	$(".float_2r").toggle(
-  		function () {
-    		$(".float2").addClass("fright");
-    		$(".float_2r").html("2号无浮动")
- 		},
-  		function () {
-    		$(".float2").removeClass("fright");
-    		$(".float_2r").html("2号右浮动")
-  	});
-  	$(".float_3l").toggle(
-  		function () {
-    		$(".float3").addClass("fleft");
-    		$(".float_3l").html("3号无浮动")
- 		},
-  		function () {
-    		$(".float3").removeClass("fleft");
-    		$(".float_3l").html("3号左浮动")
-  	});
-  	$(".float_3r").toggle(
-  		function () {
-    		$(".float3").addClass("fright");
-    		$(".float_3r").html("3号无浮动")
- 		},
-  		function () {
-    		$(".float3").removeClass("fright");
-    		$(".float_3r").html("3号右浮动")
+        $(this).siblings(".float_demo").removeClass("fleft");
   	});
 });
