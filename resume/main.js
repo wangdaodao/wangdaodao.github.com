baecc54d8a4e7e5a550221141c08ab36631a1da0
wangdaodao <hi@wangdaodao.com>
2014-02-12 13:47:38 +0800

    简历迁移
    简历迁移

---
+++ resume/main.js
@@ -0,0 +1,1 @@
+try {if(window.console && console.log('%c', 'padding:215px 215px;line-height:430px;height:1px;background:url(http://wangdaodao.github.com/images/mmqrcode1390629132792.png) 0 0 no-repeat;'));}catch (e){};
