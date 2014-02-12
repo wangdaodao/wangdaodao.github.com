e43e7e61128267bd27221bfe423d05492746d4ef
wangdaodao <hi@wangdaodao.com>
2014-02-12 15:38:05 +0800

    删除博客
    删除博客

--- javascripts/config.js
+++
@@ -1,6 +0,0 @@
-var disqus_shortname = 'wangdaodao';
-var hostbase = 'http://wangdaodao.github.io';
-var githubname = 'wangdaodao';
-var repos = 'wangdaodao.github.io';
-var sitetitle = '王叨叨';
-var rss = '';
