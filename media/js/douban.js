6382d63b93c635c02d2dd04b41c7a98a484758e1
wangdaodao <hi@wangdaodao.com>
2014-06-04 14:21:43 +0800

    05236daf832df7500f6a490e8989e5f0

--- media/js/douban.js
+++ media/js/douban.js
@@ -2,7 +2,7 @@ function DoubanApi() {
     this.defaults = {
         place:"douban",
         user:"wang_daodao",
-        api:"0b204112100ace0a2160c33630206a1e",
+        api:"05236daf832df7500f6a490e8989e5f0",
         book:[{status:"reading",maxnum:100},{status:"read",maxnum:100},{status:"wish",maxnum:100}],
         bookreadingtitle:"在读...",
         bookreadtitle:"读过...",
