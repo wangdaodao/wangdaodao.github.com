06bc7c35399cf83f16890ea4fb3efbe8fa59173f
wangdaodao <hi@wangdaodao.com>
2014-08-18 13:42:24 +0800

    post

--- media/kill-ie6/script-ie6.js
+++
@@ -1,8 +0,0 @@
-/* Kill IE6 */
-
-;(function() {
-    var killie6 = document.createElement('div');
-    killie6.id = 'killie6';
-    killie6.innerHTML = '<div class="killie6r4"></div><div class="killie6r2"></div><div class="killie6r1"></div><div class="killie6r1"></div><div class="killie6c"><span class="killie6pic"></span><p class="killie6text">您正在使用 Internet Explorer 6 上网。如果您 <strong>升级到 Internet Explorer 8</strong> 或 <strong>转换到另一款浏览器</strong>，将会得到更好的浏览体验。</p><p class="killie6browsers"><a rel="nofollow" class="killie6ie8" href="http://windows.microsoft.com/zh-CN/internet-explorer/products/ie/home">IE 8</a><a rel="nofollow" class="killie6chrome" href="http://www.google.cn/chrome/">Chrome</a><a rel="nofollow" class="killie6firefox" href="http://firefox.com.cn/">Firefox</a><a rel="nofollow" class="killie6opera" href="http://www.opera.com/browser/">Opera</a></p></div><div class="killie6r1"></div><div class="killie6r1"></div><div class="killie6r2"></div><div class="killie6r4"></div>';
-    document.body.appendChild(killie6);
-})();
